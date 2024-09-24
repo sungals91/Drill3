d8fe429b588ed03c44b1febc67d54dafaddfb2d7
sungals91 <sungals911@gmail.com>
2024-09-24 10:37:48 +0900

    사각형 그리기 뼈대 완성 & 원운동 잠시 중단

--- char_rectangle_circle_moving.py
+++ char_rectangle_circle_moving.py
@@ -6,8 +6,29 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_top():
+    print('top')
+    pass
+
+def run_right():
+    print('right')
+    pass
+
+def run_bottom():
+    print('bottom')
+    pass
+
+def run_left():
+    print('left')
+    pass
+
 def run_rectangle():
     print('rectangle')
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
+    
     pass
 
 def run_circle():
@@ -22,11 +43,10 @@ def run_circle():
         clear_canvas_now()
         character.draw_now(x,y)
         delay(0.01)
-        pass
 
 
 while True:
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
 
