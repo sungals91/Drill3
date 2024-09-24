e5ad812311b42800a02d9f90f95a54d5e2e0a566
sungals91 <sungals911@gmail.com>
2024-09-24 09:53:45 +0900

    print로 루프 정상 작동 확인함

--- char_rectangle_circle_moving.py
+++ char_rectangle_circle_moving.py
@@ -1,9 +1,11 @@
 from pico2d import *
 
 def run_rectangle():
+    print('rectangle')
     pass
 
 def run_circle():
+    print('circle')
     pass
 
 open_canvas()
@@ -12,7 +14,7 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 while True:
-    run_rectangle()
     run_circle()
+    run_rectangle()
 
 close_canvas()
