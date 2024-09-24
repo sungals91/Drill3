2720814f3855219c7ba0dc1f5b60bdedfee9f9e0
sungals91 <sungals911@gmail.com>
2024-09-24 10:01:57 +0900

    캐릭터 출력

--- char_rectangle_circle_moving.py
+++ char_rectangle_circle_moving.py
@@ -1,20 +1,24 @@
 from pico2d import *
 
-def run_rectangle():
-    print('rectangle')
-    pass
-
-def run_circle():
-    print('circle')
-    pass
-
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def run_rectangle():
+    print('rectangle')
+    pass
+
+def run_circle():
+    clear_canvas_now()
+    character.draw_now(400,300)
+    delay(1)
+    pass
+
+
 while True:
     run_circle()
     run_rectangle()
+    break
 
 close_canvas()
